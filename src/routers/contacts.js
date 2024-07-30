85408f70d795cd26f6eb58e8f6dd45760a1cdeb7
OlenaHlu <glushenkova_es@ukr.net>
2024-07-30 20:59:20 +0300

    fix

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -27,7 +27,7 @@ router.get('/', ctrlWrapper(getContactsController));
 router.get('/:contactId', isValidId, ctrlWrapper(getContactByIdController));
 
 router.post(
-  '/register',
+  '',
   jsonParser,
   validateBody(createContactSchema),
   ctrlWrapper(createContactController),
